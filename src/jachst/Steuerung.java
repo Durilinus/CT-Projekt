f1a7feee8b27acbfe55ac24727fa41b0558641c0
Christian.Metzger <Christian.Metzger@R26-PC15.schule.local>
2018-12-07 16:53:23 +0100

    Merge origin/master
    Conflicts:
    	src/jachst/JachstFrame.form
    	src/jachst/JachstFrame.java

--- src/jachst/Steuerung.java
+++ src/jachst/Steuerung.java
@@ -6,6 +6,11 @@
 package jachst;
 
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 
 /**
  *
@@ -16,9 +21,17 @@ public class Steuerung {
     private Held rolf;
     private GUI dieGUI;
     private Bodenmobs dieBodenMobs[];
+    private Timer t1;
+    private Rectangle hitboxenBodenMobs[];
+    private Rectangle hitboxenFlugMobs[];
+    private Rectangle hitboxHeld;
+    
+    private static int kastenBreite;
+    private static int kastenHoehe;
+    private static final int KASTENMENGEX = 10;
+    private static final int KASTENMENGEY = 6;
     
     Steuerung(GUI gui){
-       
         dieGUI = gui;
         neuesSpiel();
         
@@ -27,13 +40,31 @@ public class Steuerung {
         initFiguren();
         rolf.setStartPos();
         dieGUI.repaint();
-        
+        t1 = new Timer(10, new ActionListener(){
+              public void actionPerformed(ActionEvent ae){
+                  bewegeAlleMobs();   
+              }
+            });
+        t1.start();    
+    }
+    public void bewegeAlleMobs(){
+        for (int i = 0; i < dieFlugMobs.length; i++) {
+            dieFlugMobs[i].laufen();   
+        }
+        for (int i = 0; i < dieBodenMobs.length; i++) {
+            dieBodenMobs[i].laufen();
+            
+        }
     }
     public void zeichneAlles(Graphics g){
         rolf.zeichne(g);
     }
+    
     public void initFiguren(){
-       rolf=new Held();
+       kastenBreite = dieGUI.getWidth() / KASTENMENGEX;
+       kastenHoehe = dieGUI.getHeight() / KASTENMENGEY;
+        
+       rolf = new Held();
        dieFlugMobs = new Flugmobs[10];
        for(int i = 0; i < dieFlugMobs.length; i++){
            dieFlugMobs[i] = new Flugmobs();
@@ -41,10 +72,10 @@ public class Steuerung {
        dieBodenMobs = new Bodenmobs[10];
        for(int i = 0; i < dieBodenMobs.length; i++){
            dieBodenMobs[i] = new Bodenmobs();
-       }
-       
-       
-       
+       }   
+    }
+    public void initHitboxen(){
+        hitboxHeld = new Rectangle(rolf.pX,rolf.pY, kastenBreite, kastenHoehe);
     }
     
     
